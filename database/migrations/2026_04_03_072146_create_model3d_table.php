431cae369e28e8c83174434216850006ad90cfcc
Z3ND3J4S <s4tr11con1@gmail.com>
2026-04-03 01:38:17 -0600

    feat: initialize models, controllers, and database migrations for Model3D, Category, Comment, and StoreCart entities

---
+++ database/migrations/2026_04_03_072146_create_model3d_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('model3d', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description');
+            $table->string('file');
+            $table->decimal('price', 10, 2);
+
+            $table->foreignId('id_category')->references('id')->on('categories')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('model3d');
+    }
+};
